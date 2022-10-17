9c2253d19b86eec308479949debfa7468acffa74
Luvie Rallos <luvierallos@gmail.com>
2022-10-17 15:32:39 +0800

    add basic layout of the functions

---
+++ script.js
@@ -0,0 +1,25 @@
+const startX = document.getElementById('play-x');
+const startO = document.getElementById('play-o');
+
+
+
+
+//press one of the play buttons
+//highlight the pressed button
+//pressing the buttons during live play resets it as the pressed button
+function startGame() {
+    
+}
+
+
+//code runs when a cell is clicked
+//puts a letter on the cell
+function chooseCell(){
+
+}
+
+
+//checks the win condition
+function winCondition{
+
+}
