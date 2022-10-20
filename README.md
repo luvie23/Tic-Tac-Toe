f145ba8b617c142687b619e3a590abe2e8307e71
Luvie Rallos <luvierallos@gmail.com>
2022-10-20 08:21:15 +0800

    add screenshot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tic-Tac-Toe
+
+
+
+![screenshot](https://raw.githubusercontent.com/luvie23/Tic-Tac-Toe/master/screengrab.png)
--- script.js
+++ script.js
@@ -77,14 +77,8 @@ function computerMove(){
 
 }
 
-const winOptions = [
-    [0,1,2],[3,4,5],[6,7,8],[0,3,6],
-    [1,4,7],[2,5,8],[0,4,8],[2,4,6]
 
-]
 //checks the win condition
-
-
 function winner(winner) {
     won.classList.remove('hidden')
     gameBox.classList.add('hidden')
